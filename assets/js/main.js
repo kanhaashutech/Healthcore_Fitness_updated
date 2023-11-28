961f6200aaa6218330abc20a23af743ffdf1bdca
kanhaashutech <kashuexpert@gmail.com>
2023-11-28 18:25:35 +0530

    fdxgb

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,20 @@
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click', () =>{
+if (navClose) {
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
@@ -22,7 +22,7 @@ if(navClose){
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
 
-const linkAction = () =>{
+const linkAction = () => {
     const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
@@ -31,43 +31,43 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-const scrollHeader = () =>{
+const scrollHeader = () => {
     const header = document.getElementById('header')
     // When the scroll is greater than 50 viewport height, add the bg-header class to the header tag
-    this.scrollY >= 50 ? header.classList.add('bg-header') 
-                       : header.classList.remove('bg-header')
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+        : header.classList.remove('bg-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
-    
-const scrollActive = () =>{
-  	const scrollY = window.pageYOffset
 
-	sections.forEach(current =>{
-		const sectionHeight = current.offsetHeight,
-			  sectionTop = current.offsetTop - 58,
-			  sectionId = current.getAttribute('id'),
-			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+const scrollActive = () => {
+    const scrollY = window.pageYOffset
 
-		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-			sectionsClass.classList.add('active-link')
-		}else{
-			sectionsClass.classList.remove('active-link')
-		}                                                    
-	})
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
 }
 window.addEventListener('scroll', scrollActive)
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
-const scrollUp = () =>{
-	const scrollUp = document.getElementById('scroll-up')
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
     // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
-	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-						: scrollUp.classList.remove('show-scroll')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
 
@@ -80,22 +80,22 @@ const sr = ScrollReveal({
 })
 
 sr.reveal(`.home__data, .footer__container, .footer__group`)
-sr.reveal(`.home__img`, {delay: 700, origin: 'bottom'})
-sr.reveal(`.logos__img, .program__card, .pricing__card`, {interval: 100})
-sr.reveal(`.choose__img, .calculate__content`, {origin: 'left'})
-sr.reveal(`.choose__content, .calculate__img`, {origin: 'right'})
+sr.reveal(`.home__img`, { delay: 700, origin: 'bottom' })
+sr.reveal(`.logos__img, .program__card, .pricing__card`, { interval: 100 })
+sr.reveal(`.choose__img, .calculate__content`, { origin: 'left' })
+sr.reveal(`.choose__content, .calculate__img`, { origin: 'right' })
 
 /*=============== CALCULATE JS ===============*/
 const calculateForm = document.getElementById('calculate-form'),
-      calculateCm = document.getElementById('calculate-cm'),
-      calculateKg = document.getElementById('calculate-kg'),
-      calculateMessage = document.getElementById('calculate-message')
+    calculateCm = document.getElementById('calculate-cm'),
+    calculateKg = document.getElementById('calculate-kg'),
+    calculateMessage = document.getElementById('calculate-message')
 
-const calculateBmi = (e) =>{
+const calculateBmi = (e) => {
     e.preventDefault()
-    
+
     // Check if the fields have a value
-    if(calculateCm.value === '' || calculateKg.value === ''){
+    if (calculateCm.value === '' || calculateKg.value === '') {
         // Add and remove color
         calculateMessage.classList.remove('color-green')
         calculateMessage.classList.add('color-red')
@@ -107,21 +107,21 @@ const calculateBmi = (e) =>{
         setTimeout(() => {
             calculateMessage.textContent = ''
         }, 3000);
-    }else{
+    } else {
         // BMI Formula
         const cm = calculateCm.value / 100,
-              kg = calculateKg.value,
-              bmi = Math.round(kg / (cm * cm))
+            kg = calculateKg.value,
+            bmi = Math.round(kg / (cm * cm))
 
         // Show your health status
-        if(bmi < 18.5){
+        if (bmi < 18.5) {
             // Add color and display message
             calculateMessage.classList.add('color-green')
             calculateMessage.textContent = `Your BMI is ${bmi} and you are skinny 😔`
-        } else if(bmi < 25){
+        } else if (bmi < 25) {
             calculateMessage.classList.add('color-green')
             calculateMessage.textContent = `Your BMI is ${bmi} and you are healthy 🥳`
-        }else{
+        } else {
             calculateMessage.classList.add('color-green')
             calculateMessage.textContent = `Your BMI is ${bmi} and you are overweight 😔`
         }
@@ -141,26 +141,26 @@ calculateForm.addEventListener('submit', calculateBmi)
 
 /*=============== EMAIL JS ===============*/
 const contactForm = document.getElementById('contact-form'),
-      contactMessage = document.getElementById('contact-message'),
-      contactUser = document.getElementById('contact-user')
+    contactMessage = document.getElementById('contact-message'),
+    contactUser = document.getElementById('contact-user')
 
-const sendEmail = (e) =>{
+const sendEmail = (e) => {
     e.preventDefault()
 
     // Check if the field has a value
-    if(contactUser.value === ''){
+    if (contactUser.value === '') {
         // Add and remove color
         contactMessage.classList.remove('color-green')
         contactMessage.classList.add('color-red')
 
         // Show message
         contactMessage.textContent = 'You must enter your email ☝'
-        
+
         // Remove message three seconds
         setTimeout(() => {
             contactMessage.textContent = ''
         }, 3000);
-    }else{
+    } else {
         // serviceID - templateID - #form - publicKey
         emailjs.sendForm('', '', '#contact-form', '')
             .then(() => {
@@ -182,3 +182,24 @@ const sendEmail = (e) =>{
     }
 }
 contactForm.addEventListener('submit', sendEmail)
+
+
+//  popup form
+
+// const popupForm = document.querySelector(".contact-popup");
+// const closeForm = document.getElementById('*formpp-close'); xzy
+// const joinNow = document.getElementById('joinNow');
+
+// joinNow.addEventListener("click", form);
+
+// function form() {
+//     popupForm.style.display = "block";
+// }
+
+// closeForm.addEventListener("click", close);
+
+// function close() {
+
+//     popupForm.style.display = "none";
+
+// }
